74f170682c3db422afcd72bafe7fa574380edaf8
Jeff <jm@jeffz.com>
2020-11-14 01:39:03 -0500

    Added Member model

---
+++ YarnsAndMobile/Models/Member.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YarnsAndMobile.Models
+{
+    public class Member
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string AccountNumber { get; set; }
+        public virtual Address Address { get; set; }
+        public virtual IEnumerable<Phone> Phones { get; set; }
+        public virtual IEnumerable<Sale> Sales { get; set; }
+        public virtual IEnumerable<Review> Reviews { get; set; }
+    }
+}
