a65de1338d95eeb7abe09740c68aa52f2366fc6d
Jeff <jm@jeffz.com>
2020-11-14 01:46:39 -0500

    Added Review model

---
+++ YarnsAndMobile/Models/Review.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YarnsAndMobile.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime Date { get; set; }
+        public int Rating { get; set; }
+        public virtual Member Member { get; set; }
+        public virtual Book Book { get; set; }
+    }
+}
