a96cc1ea6f297b02608a685307e259dcd57994e8
Jeff <jm@jeffz.com>
2020-11-14 01:45:42 -0500

    Added PhoneType model

---
+++ YarnsAndMobile/Models/PhoneType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YarnsAndMobile.Models
+{
+    public class PhoneType
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+    }
+}
