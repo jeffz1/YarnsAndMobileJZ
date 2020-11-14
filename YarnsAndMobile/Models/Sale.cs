f9a4815d18666b4471bc46baaab9b552cfc2e930
Jeff <jm@jeffz.com>
2020-11-14 01:46:09 -0500

    Added Sale model

---
+++ YarnsAndMobile/Models/Sale.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YarnsAndMobile.Models
+{
+    public class Sale
+    {
+        public int Id { get; set; }
+        public decimal Price { get; set; }
+        public DateTime Date { get; set; }
+        public virtual Member Member { get; set; }
+        public virtual Book Book { get; set; }
+
+    }
+}
