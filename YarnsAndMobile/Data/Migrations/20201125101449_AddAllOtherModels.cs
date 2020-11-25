8c4704cec95b86aec9533bcccab7e8e9d1ee2532
Jeff <jm@jeffz.com>
2020-11-25 05:19:10 -0500

    Migrate remaining models

---
+++ YarnsAndMobile/Data/Migrations/20201125101449_AddAllOtherModels.cs
@@ -0,0 +1,464 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace YarnsAndMobile.Data.Migrations
+{
+    public partial class AddAllOtherModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Address_AddressId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Phone_AspNetUsers_MemberId",
+                table: "Phone");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Phone_PhoneType_TypeId",
+                table: "Phone");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Review_Books_BookId",
+                table: "Review");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Review_AspNetUsers_MemberId",
+                table: "Review");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sale_Books_BookId",
+                table: "Sale");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sale_AspNetUsers_MemberId",
+                table: "Sale");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Sale",
+                table: "Sale");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Review",
+                table: "Review");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_PhoneType",
+                table: "PhoneType");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Phone",
+                table: "Phone");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Address",
+                table: "Address");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "AspNetUsers");
+
+            migrationBuilder.RenameTable(
+                name: "Sale",
+                newName: "Sales");
+
+            migrationBuilder.RenameTable(
+                name: "Review",
+                newName: "Reviews");
+
+            migrationBuilder.RenameTable(
+                name: "PhoneType",
+                newName: "PhoneTypes");
+
+            migrationBuilder.RenameTable(
+                name: "Phone",
+                newName: "Phones");
+
+            migrationBuilder.RenameTable(
+                name: "Address",
+                newName: "Addresses");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Sale_MemberId",
+                table: "Sales",
+                newName: "IX_Sales_MemberId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Sale_BookId",
+                table: "Sales",
+                newName: "IX_Sales_BookId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Review_MemberId",
+                table: "Reviews",
+                newName: "IX_Reviews_MemberId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Review_BookId",
+                table: "Reviews",
+                newName: "IX_Reviews_BookId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Phone_TypeId",
+                table: "Phones",
+                newName: "IX_Phones_TypeId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Phone_MemberId",
+                table: "Phones",
+                newName: "IX_Phones_MemberId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AccountNumber",
+                table: "AspNetUsers",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Sales",
+                table: "Sales",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Reviews",
+                table: "Reviews",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_PhoneTypes",
+                table: "PhoneTypes",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Phones",
+                table: "Phones",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Addresses",
+                table: "Addresses",
+                column: "Id");
+
+            migrationBuilder.InsertData(
+                table: "PhoneTypes",
+                columns: new[] { "Id", "Description" },
+                values: new object[,]
+                {
+                    { 1, "Mobile" },
+                    { 2, "Home" },
+                    { 3, "Work" },
+                    { 4, "School" },
+                    { 5, "Emergency Contact" }
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Addresses_AddressId",
+                table: "AspNetUsers",
+                column: "AddressId",
+                principalTable: "Addresses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Phones_AspNetUsers_MemberId",
+                table: "Phones",
+                column: "MemberId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Phones_PhoneTypes_TypeId",
+                table: "Phones",
+                column: "TypeId",
+                principalTable: "PhoneTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_Books_BookId",
+                table: "Reviews",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "BookId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_AspNetUsers_MemberId",
+                table: "Reviews",
+                column: "MemberId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sales_Books_BookId",
+                table: "Sales",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "BookId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sales_AspNetUsers_MemberId",
+                table: "Sales",
+                column: "MemberId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Addresses_AddressId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Phones_AspNetUsers_MemberId",
+                table: "Phones");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Phones_PhoneTypes_TypeId",
+                table: "Phones");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_Books_BookId",
+                table: "Reviews");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_AspNetUsers_MemberId",
+                table: "Reviews");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sales_Books_BookId",
+                table: "Sales");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sales_AspNetUsers_MemberId",
+                table: "Sales");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Sales",
+                table: "Sales");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Reviews",
+                table: "Reviews");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_PhoneTypes",
+                table: "PhoneTypes");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Phones",
+                table: "Phones");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Addresses",
+                table: "Addresses");
+
+            migrationBuilder.DeleteData(
+                table: "PhoneTypes",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "PhoneTypes",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "PhoneTypes",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "PhoneTypes",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "PhoneTypes",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.RenameTable(
+                name: "Sales",
+                newName: "Sale");
+
+            migrationBuilder.RenameTable(
+                name: "Reviews",
+                newName: "Review");
+
+            migrationBuilder.RenameTable(
+                name: "PhoneTypes",
+                newName: "PhoneType");
+
+            migrationBuilder.RenameTable(
+                name: "Phones",
+                newName: "Phone");
+
+            migrationBuilder.RenameTable(
+                name: "Addresses",
+                newName: "Address");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Sales_MemberId",
+                table: "Sale",
+                newName: "IX_Sale_MemberId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Sales_BookId",
+                table: "Sale",
+                newName: "IX_Sale_BookId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Reviews_MemberId",
+                table: "Review",
+                newName: "IX_Review_MemberId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Reviews_BookId",
+                table: "Review",
+                newName: "IX_Review_BookId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Phones_TypeId",
+                table: "Phone",
+                newName: "IX_Phone_TypeId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Phones_MemberId",
+                table: "Phone",
+                newName: "IX_Phone_MemberId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AccountNumber",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Sale",
+                table: "Sale",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Review",
+                table: "Review",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_PhoneType",
+                table: "PhoneType",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Phone",
+                table: "Phone",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Address",
+                table: "Address",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Address_AddressId",
+                table: "AspNetUsers",
+                column: "AddressId",
+                principalTable: "Address",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Phone_AspNetUsers_MemberId",
+                table: "Phone",
+                column: "MemberId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Phone_PhoneType_TypeId",
+                table: "Phone",
+                column: "TypeId",
+                principalTable: "PhoneType",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Review_Books_BookId",
+                table: "Review",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "BookId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Review_AspNetUsers_MemberId",
+                table: "Review",
+                column: "MemberId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sale_Books_BookId",
+                table: "Sale",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "BookId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sale_AspNetUsers_MemberId",
+                table: "Sale",
+                column: "MemberId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- YarnsAndMobile/Data/YamDbContext.cs
+++ YarnsAndMobile/Data/YamDbContext.cs
@@ -4,14 +4,31 @@ using YarnsAndMobile.Models;
 
 namespace YarnsAndMobile.Data
 {
-    public class YamDbContext : IdentityDbContext
+    public class YamDbContext : IdentityDbContext<Member>
     {
         public YamDbContext(DbContextOptions<YamDbContext> options) : base(options)
         {            
         }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<PhoneType>().HasData(
+                    new PhoneType { Id = 1, Description = "Mobile"},
+                    new PhoneType { Id = 2, Description = "Home"},
+                    new PhoneType { Id = 3, Description = "Work"},
+                    new PhoneType { Id = 4, Description = "School"},
+                    new PhoneType { Id = 5, Description = "Emergency Contact"}
+                );
+            base.OnModelCreating(builder);
+        }
+
         public DbSet<Member> Members { get; set; }
         public DbSet<Book> Books { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Phone> Phones { get; set; }
+        public DbSet<PhoneType> PhoneTypes { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Sale> Sales { get; set; }
 
     }
 }
