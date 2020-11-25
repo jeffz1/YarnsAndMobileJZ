e2713745ee3635ed082fe2569590570d5ebc7db3
Jeff <jm@jeffz.com>
2020-11-25 04:37:33 -0500

    Added migration for AddSalePriceToBook

---
+++ YarnsAndMobile/Data/Migrations/20201124224859_AddSalePriceToBook.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace YarnsAndMobile.Data.Migrations
+{
+    public partial class AddSalePriceToBook : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "CurrentSalePrice",
+                table: "Books",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CurrentSalePrice",
+                table: "Books");
+        }
+    }
+}
