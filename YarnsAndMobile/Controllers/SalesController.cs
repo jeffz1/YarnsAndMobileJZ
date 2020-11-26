ef02cee333bf5cdd87688623cee7d5a9da1ab265
Jeff <jm@jeffz.com>
2020-11-26 15:17:55 -0500

    Added initial (scaffolded) Sales Controller and Views plus feature to buy a book

---
+++ YarnsAndMobile/Controllers/SalesController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using YarnsAndMobile.Data;
+using YarnsAndMobile.Models;
+
+namespace YarnsAndMobile.Controllers
+{
+    [Authorize]
+    public class SalesController : Controller
+    {
+        private readonly YamDbContext _context;
+        private readonly UserManager<Member> _userManager;
+
+        public SalesController(YamDbContext context, UserManager<Member> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET: Sales
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Sales.OrderByDescending(s=>s.Date).ToListAsync());
+        }
+
+        // GET: Sales/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sale = await _context.Sales
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (sale == null)
+            {
+                return NotFound();
+            }
+
+            return View(sale);
+        }
+
+        // GET: Sales/Create
+        public IActionResult Create(int id)
+        {
+            var book = _context.Books.Find(id);
+            var sale = new Sale
+            {
+                Book = book,
+                Price = book?.CurrentSalePrice ?? 0,
+                Date = DateTime.Now,
+                Member = _userManager.GetUserAsync(User).Result
+            };
+            return View(sale);
+        }
+
+        // POST: Sales/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Sale sale)
+        {
+            //sale.Book = _context.Books.Find(sale.Book.BookId);
+            if (ModelState.IsValid)
+            {
+                sale.Member = _context.Members.Find(_userManager.GetUserId(User));
+                sale.Book = _context.Books.Find(sale.Book.BookId);
+                sale.Id = 0;
+                _context.Add(sale);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(sale);
+        }
+
+        // GET: Sales/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sale = await _context.Sales.FindAsync(id);
+            if (sale == null)
+            {
+                return NotFound();
+            }
+            return View(sale);
+        }
+
+        // POST: Sales/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Price,Date")] Sale sale)
+        {
+            if (id != sale.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(sale);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SaleExists(sale.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(sale);
+        }
+
+        // GET: Sales/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sale = await _context.Sales
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (sale == null)
+            {
+                return NotFound();
+            }
+
+            return View(sale);
+        }
+
+        // POST: Sales/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var sale = await _context.Sales.FindAsync(id);
+            _context.Sales.Remove(sale);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SaleExists(int id)
+        {
+            return _context.Sales.Any(e => e.Id == id);
+        }
+    }
+}
